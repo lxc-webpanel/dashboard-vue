b19f419a61808be86c09685ae5a3da611a6386dc
Antoine <antoine@tanzil.li>
2018-01-09 14:20:22 +0100

    Lock dependencies versions

--- src/libraries/utils/axios.js
+++ src/libraries/utils/axios.js
@@ -1,13 +1,3 @@
-/**
- * Custom axios instance
- * > - [Axios的配置](https://blog.ygxdxx.com/2017/01/29/Axios-Config/)
- * > - [Vuex2和Axios的开发](https://blog.ygxdxx.com/2017/02/01/Vuex2&Axios-Develop/)
- * > - [Axios全攻略](https://blog.ygxdxx.com/2017/02/27/Axios-Strategy/)
- * > - [Vue 全家桶 + axios 前端实现登录拦截、登出、拦截器等功能](https://github.com/superman66/vue-axios-github)
- * > - [axios和网络传输相关知识的学习实践](http://www.jianshu.com/p/8e5fb763c3d7)
- * > - [Vue.js REST API Consumption with Axios](https://alligator.io/vuejs/rest-api-axios/)
- */
-
 import axios from 'axios';
 import normalize from 'json-api-normalizer';
 import storage from './storage';
